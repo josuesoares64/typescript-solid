2c477d8e645ee84aa606fae30c854978152f8f96
Josué Bezerra Soares <josue.bezerra.2020@gmail.com>
2026-03-10 15:58:02 -0300

    Finaliza lógica de validação da tarefa 5

--- tarefa-5/main.ts
+++ tarefa-5/main.ts
@@ -1,7 +1,8 @@
 import ClienteController from "./controller/ClienteController";
 import Cliente from "./entities/Cliente";
+import PostgresRepository from "./repositories/PostgresRepository";
 
-const clienteController = new ClienteController();
+const clienteController = new ClienteController(new PostgresRepository());
 
 const cliente1 = new Cliente(0, "João", "joao@mail.com")
 const cliente2 = new Cliente(0, "Kleber", "kleber@mail.com")
